20e7aebbab64f83393198c62f07838ead6ec755c
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-03-31 07:20:04 +0530

    components

---
+++ src/components/AddContact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class AddContact extends React.Component { 
+    render() {
+        return (
+
+            <div></div>
+        );
+    }
+}
+
+export default AddContact
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactList = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default ContactList
